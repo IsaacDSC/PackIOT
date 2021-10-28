4197fa817027e431c3fb67603daf3f9ae19421f7
IsaacDSC <56350331+IsaacDSC@users.noreply.github.com>
2021-10-28 15:31:18 -0300

    create users patterns

--- initializeDatabase.js
+++ initializeDatabase.js
@@ -1,6 +1,7 @@
 const { LinesProductions } = require('./src/database/models/linesProductions')
 const { monitorLines } = require('./src/database/models/monitorLines')
 const { Account } = require('./src/database/models/accounts')
+const AccountProcedules = require('./src/database/procedules/AccountsProcedules')
 
 const TABLES = [LinesProductions, monitorLines, Account]
 
@@ -15,8 +16,8 @@ const initialize = async(active) => {
 //initialize()
 const createUser = async() => {
     // Account.create({ username: 'Administrador', email: 'granadophebo.com.br', password: 'packIOT' })
-    Account.create({ username: 'acrocha', email: 'acrocha@granadophebo.com.br', password: 'joaquim.2018' })
-    Account.create({ username: 'm.oros', email: 'All@12345@granadophebo.com.br', password: 'All@12345' })
+    AccountProcedules.create('acrocha', 'acrocha@granadophebo.com.br', 'joaquim.2018')
+    AccountProcedules.create('m.oros', 'All@12345@granadophebo.com.br', 'All@12345')
 }
 
 
